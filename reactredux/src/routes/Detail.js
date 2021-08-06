e8dd08182473a889ee84e834e5417c573619e042
이지호 <cctv1998@naver.com>
2021-08-06 18:23:55 +0900

    Finish

--- reactredux/src/routes/Detail.js
+++ reactredux/src/routes/Detail.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { connect } from "react-redux";
 
-function Detail() {
-    return (
-        <div>
-            
-        </div>
-    )
+function Detail({ toDo }) {
+  return (
+    <>
+      <h1>{toDo?.text}</h1>
+      <h5>Created at: {toDo?.id}</h5>
+    </>
+  );
 }
 
-export default Detail
+function mapStateToProps(state, ownProps) {
+  const {
+    match: {
+      params: { id }
+    }
+  } = ownProps;
+  return { toDo: state.find(toDo => toDo.id === parseInt(id)) };
+}
+
+export default connect(mapStateToProps)(Detail);
