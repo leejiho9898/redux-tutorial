afa8c8be818df45f341bd633401ae476f929401b
이지호 <cctv1998@naver.com>
2021-08-07 13:39:31 +0900

    Add : reduxtoolkit

--- reactredux/src/components/ToDo.js
+++ reactredux/src/components/ToDo.js
@@ -3,12 +3,11 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { actionCreators } from "./../store";
 
-function ToDo({ text, onBtnClick ,id}) {
+function ToDo({ text, onBtnClick, id }) {
   return (
     <li>
-      <Link to={`/${id}`}>
-        {text} <button onClick={onBtnClick}>DEL</button>
-      </Link>
+      <Link to={`/${id}`}>{text}</Link>
+      <button onClick={onBtnClick}>DEL</button>
     </li>
   );
 }
--- reactredux/src/store.js
+++ reactredux/src/store.js
@@ -1,40 +1,54 @@
 import { createStore } from "redux";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+// const ADD = "ADD";
+// const DELETE = "DELETE";
+// const addToDo = (text) => {
+//   return {
+//     type: ADD,
+//     text,
+//   };
+// };
+// const deleteToDo = (id) => {
+//   return {
+//     type: DELETE,
+//     id : parseInt(id)
+//   };
+// };
 
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id : parseInt(id)
-  };
-};
- 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
+//리덕스 툴킷을 이용하면 이걸로 대체가능
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
-const store = createStore(reducer);
 
-export const actionCreators ={
+
+
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       console.log(action.payload)
+//       return state.filter((toDo) => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+//얘도리덕스 툴킷으로 바꾸면
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) => {
+    state.filter(toDo => toDo.id !== action.payload)
+  },
+});
+
+const store = configureStore({reducer});
+
+export const actionCreators = {
   addToDo,
-  deleteToDo
-}
-
+  deleteToDo,
+};
 
 export default store;
-
-
